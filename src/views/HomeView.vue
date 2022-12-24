f62910258b030bf934c963261a35dbc656b06564
Adit Tanu <adiettanu27@gmail.com>
2022-12-24 14:58:17 +0700

    watch vs watchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,39 @@
 <template>
-    <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">Update Ninja Two</button>
+    <div class="home">
+        <h1>Home</h1>
+        <input type="text" v-model="search" />
+        <p>Search term - {{ search }}</p>
+        <div v-for="name in matchingNames" :key="name">
+            <p>{{ name }}</p>
+        </div>
+        <button @click="handleClick">Stop watching</button>
+    </div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity"
+import { computed, ref, watch, watchEffect } from "@vue/runtime-core"
 export default {
     name: "Home",
     setup() {
-        const ninjaOne = ref({
-            name: "Mario",
-            age: 30,
-        })
-        const ninjaTwo = reactive({
-            name: "luigi",
-            age: 35,
-        })
-        const nameOne = ref("mario")
-        const nameTwo = reactive("luigi")
+        const names = ref(["mario", "yoshi", "luigi", "toad"])
+        const search = ref("")
 
-        const updateNinjaOne = () => {
-            ninjaOne.value.age = 40
+        const stopWatch = watch(search, () => {
+            console.log("watch function run")
+        })
+
+        const stopEffect = watchEffect(() => console.log(search.value))
+
+        const matchingNames = computed(() => {
+            return names.value.filter((name) => name.includes(search.value))
+        })
+
+        const handleClick = () => {
+            stopWatch()
+            stopEffect()
         }
 
-        const updateNinjaTwo = () => {
-            ninjaTwo.age = 45
-            nameTwo = "mario" 
-        }
-
-        return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo, nameTwo }
+        return { names, search, matchingNames, handleClick }
     },
 }
 </script>
